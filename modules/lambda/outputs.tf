68c4f8b650dda7667065ee807a9013f2fad8c356
vitalii-pasechnyk <w.pasechnyk@gmail.com>
2025-05-12 13:18:05 +0300

    all api has done

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -21,23 +21,3 @@ output "lambda_save_course_invoke_arn" {
 output "lambda_update_course_invoke_arn" {
   value = module.lambda_update_course.lambda_function_invoke_arn
 }
-
-/* output "lambda_save_course_invoke_arn" {
-  value = module.lambda_save_course.lambda_function_invoke_arn
-}
-
-output "lambda_get_all_courses_invoke_arn" {
-  value = module.lambda_get_all_courses.lambda_function_invoke_arn
-}
- 
-output "lambda_get_course_invoke_arn" {
-  value = module.lambda_get_course.lambda_function_invoke_arn
-}
-
-output "lambda_update_course_invoke_arn" {
-  value = module.lambda_update_course.lambda_function_invoke_arn
-}
-
-output "lambda_delete_course_invoke_arn" {
-  value = module.lambda_delete_course.lambda_function_invoke_arn
-} */
